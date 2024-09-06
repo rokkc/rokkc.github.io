6c2f7f83ac6e5825b7772cf2843637612d7b9153
rokkc <yashpattekar@gmail.com>
2024-09-05 23:34:26 -0700

    more progress

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-function addGroup() {
-    console.log("hi");
+var i = 1;
+addGroup();
+
+if (localStorage.getItem("bodyState") != null) {
+    //document.body.innerHTML = localStorage.getItem("bodyState");
 }
 
-$('.group').resizable({
-    handles: 'e, w'
-}).removeClass( "ui-resizable" ).draggable({containment: "window", handle: $('.groupHeader')});
+function addGroup() {
+    document.body.innerHTML +=
+        `
+        <div class="group">
+    <div class="groupHeader">
+        <div class="groupHeaderText">Today's Work</div>
+        <div class="groupHeaderControls">
+            <button class="headerControlButton" alt="addItemButton" onclick="controlPressHeader(event)">A</button>
+            <button class="headerControlButton" alt="editButtonHeader" onclick="controlPressHeader(event)">E</button>
+            <button class="headerControlButton" alt="strikethroughButtonHeader" onclick="controlPressHeader(event)">S</button>
+            <button class="headerControlButton" alt="removeGroupButton" onclick="controlPressHeader(event)">R</button>
+        </div>
+    </div>
+    <div class="groupItems">
+        <div class="groupItem">
+            <div class="itemText">Water plants</div>
+            <div class="itemControls">
+                <div class="itemControlButton" alt="editButtonItem" onclick="controlPressItem(event)">E</div>
+                <div class="itemControlButton" alt="strikethroughButton" onclick="controlPressItem(event)">S</div>
+                <div class="itemControlButton" alt="removeItemButton" onclick="controlPressItem(event)">R</div>
+            </div>
+        </div>
+        <div class="groupItem">
+            <div class="itemText">Finish math homework</div>
+            <div class="itemControls">
+                <div class="itemControlButton" alt="editButtonItem" onclick="controlPressItem(event)">E</div>
+                <div class="itemControlButton" alt="strikethroughButton" onclick="controlPressItem(event)">S</div>
+                <div class="itemControlButton" alt="removeItemButton" onclick="controlPressItem(event)">R</div>
+            </div>
+        </div>
+        <div class="groupItem">
+            <div class="itemText">Discord call for project</div>
+            <div class="itemControls">
+                <div class="itemControlButton" alt="editButtonItem" onclick="controlPressItem(event)">E</div>
+                <div class="itemControlButton" alt="strikethroughButton" onclick="controlPressItem(event)">S</div>
+                <div class="itemControlButton" alt="removeItemButton" onclick="controlPressItem(event)">R</div>
+            </div>
+        </div>
+    </div>
+        `
+
+    $('.group').draggable({containment: "window", handle: $('.groupHeader')});
+}
+
+
 
 function controlPressHeader(event) {
     console.log("banana")
+}
+
+window.setInterval(saveState, 10000)
+
+function saveState() {
+    //localStorage.setItem("bodyState", document.body.innerHTML);
+    localStorage.setItem("bodyState", null);
 }
 
 function controlPressItem(event) {
